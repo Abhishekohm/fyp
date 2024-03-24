dcac320e1e3138c2ca1bce3f5f49f333f69108cc
MITHILESH KAWALKAR <mithikawalkar@gmail.com>
2024-03-24 19:12:10 +0530

    before merge

--- app/course/[courseId]/components/courseCard.tsx
+++ app/course/[courseId]/components/courseCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function courseCard() {
   return (
-    <div>courseCard</div>
+    <div className="border-black min-h-[150px] h-[50]">courseCard</div>
   )
 }
 
--- app/dash/page.tsx
+++ app/dash/page.tsx
@@ -4,10 +4,14 @@ import CourseCard from "../course/[courseId]/components/courseCard";
 export default function dash() {
   return (
     <>
-      <Button>
+    <h2>Dashboard</h2>
+      <Button className="ml-5 mt-5">
         <a href="http://localhost:3000/instructor/courses">Instructor</a>
       </Button>
-      <CourseCard/>
+      <Button className="ml-5 mt-5">
+        <a href="http://localhost:3000/course/18">Ethical Hacking Course Card</a>
+      </Button>
+      {/* <CourseCard/> */}
     </>
   );
 }
